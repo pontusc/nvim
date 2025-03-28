4d2830c546af1f93ca3eb9b555cb7cc5a815f6a6
pontusc <pontuscurtsson@gmail.com>
2025-03-28 13:16:50 +0100

    Added lua format style

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,10 +21,7 @@ return {
     auto_install = true,
     highlight = {
       enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
+      additional_vim_regex_highlighting = false,
     },
     indent = { enable = true, disable = { 'ruby' } },
     incremental_selection = {
