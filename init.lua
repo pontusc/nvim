a9f411c23f9a24e31982fe7aca34e8f50d01183f
pontusc <pontuscurtsson@gmail.com>
2025-03-29 12:58:38 +0100

    Add floating terminal toggle

--- init.lua
+++ init.lua
@@ -54,4 +54,34 @@ require('lazy').setup {
   },
 }
 
+-- Setup toggleterm
+require('toggleterm').setup {
+  -- Size can be a number or function which returns a number
+  size = function(term)
+    if term.direction == 'horizontal' then
+      return 15
+    elseif term.direction == 'vertical' then
+      return vim.o.columns * 0.4
+    end
+  end,
+  open_mapping = [[<c-\>]], -- Ctrl+\ to toggle terminal
+  hide_numbers = true,
+  shade_terminals = true,
+  shading_factor = 2,
+  --start_in_insert = true,
+  --insert_mappings = true,
+  persist_size = true,
+  direction = 'float', -- 'vertical' | 'horizontal' | 'float' | 'tab'
+  close_on_exit = true, -- Close the terminal window when the process exits
+  shell = vim.o.shell,
+  float_opts = {
+    border = 'curved',
+    winblend = 0,
+    highlights = {
+      border = 'Normal',
+      background = 'Normal',
+    },
+  },
+}
+
 -- vim: ts=2 sts=2 sw=2 et
