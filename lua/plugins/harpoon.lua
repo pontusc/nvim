9db3cc7c56ab77020f9f5b05c3d0405fced18a26
pontusc <pontuscurtsson@gmail.com>
2025-03-30 18:45:24 +0200

    Added bind to remove current buffer from harpoon list

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,21 +10,31 @@ return {
     local function map(lhs, rhs, opts)
       vim.keymap.set('n', lhs, rhs, opts or {})
     end
+
     map('<leader>af', function()
       harpoon:list():add()
     end, { desc = 'Add file to harpoon list' })
+
+    map('<leader>rf', function()
+      harpoon:list():remove()
+    end, { desc = 'Remove current file from harpoon list' })
+
     map('<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = '[S]how harpoon [F]iles' })
+
     map('<leader>1', function()
       harpoon:list():select(1)
     end, { desc = '[G]o to harpoon [1]' })
+
     map('<leader>2', function()
       harpoon:list():select(2)
     end)
+
     map('<leader>3', function()
       harpoon:list():select(3)
     end)
+
     map('<leader>4', function()
       harpoon:list():select(4)
     end)
