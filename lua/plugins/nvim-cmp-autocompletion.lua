4ff54d7fb0d12e07d8352cd93eff05016e38b6bc
pontusc <pontuscurtsson@gmail.com>
2025-04-15 13:17:03 +0200

    Reapply "Added buffer and cmpdline plugins, attempt to fix autocomplete for aliases in command prompt"
    This reverts commit a94cbe010bcdde06ad5f420b5369975870227c2a.

--- lua/plugins/nvim-cmp-autocompletion.lua
+++ lua/plugins/nvim-cmp-autocompletion.lua
@@ -32,6 +32,8 @@ return {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
   },
   config = function()
     -- See `:help cmp`
@@ -46,6 +48,29 @@ return {
         end,
       },
       completion = { completeopt = 'menu,menuone,noinsert' },
+
+      -- `/` cmdline setup.
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      }),
+
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' },
+            },
+          },
+        }),
+      }),
 
       -- For an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`
