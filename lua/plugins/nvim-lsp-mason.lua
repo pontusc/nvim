407621a05b836e174926602747280e546ba1261a
PontusC <pontuscurtsson@gmail.com>
2025-05-11 09:07:30 +0000

    Fix double LSP attachment

--- lua/plugins/nvim-lsp-mason.lua
+++ lua/plugins/nvim-lsp-mason.lua
@@ -198,28 +198,25 @@ return {
               enable = true,
               url = "https://www.schemastore.org/api/json/catalog.json",
             },
-            validate = true,
+            validate = false,
             format = { enabled = false },
             schemas = {
               kubernetes = {
-                "*.k8s.{yml,yaml}",
-                "*.k8s.*.{yml,yaml}",
-                "*.kube.{yml,yaml}",
-                "*.kube.*.{yml,yaml}",
-                "/tmp/tmp-files/dashboard.kube.yml",
+                "*.{yml,yaml}",
               },
               ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
               ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+              ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = "roles/tasks/*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] = "*play*.{yml,yaml}",
+              ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
               ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
               ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+              ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+              ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
               ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
-                "docker-compose*.{yml,yaml}",
-                "*docker-compose*.{yml,yaml}",
-                "*docker-compose.{yml,yaml}",
-                "*docker-compose-*.{yml,yaml}",
-                "*compose*.{yml,yaml}",
-              },
+              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
             },
           },
         },
@@ -269,7 +266,7 @@ return {
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
     require("mason-lspconfig").setup({
-      automatic_enable = true,
+      automatic_enable = false,
       ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
       automatic_installation = false,
 
