7e2a88fd3a4c0eb3b124dc0dd25f4ad2490bde7e
Pontus Curtsson <pontuscurtsson@gmail.com>
2025-09-28 12:15:39 +0200

    All LSP configs

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,122 @@
+return
+-- Add all LSPs and configs
+{
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      basedpyright = {
+        settings = {
+          basedpyright = {
+            disableOrganizedImports = false,
+            analysis = {
+              typeCheckingMode = "off",
+              ignore = { "*" },
+            },
+          },
+        },
+      },
+
+      bashls = {},
+      jsonls = {},
+      ts_ls = {},
+      taplo = {},
+
+      yamlls = {
+        filetypes = { "yaml" },
+        settings = {
+          yaml = {
+            schemaStore = {
+              enable = true,
+              url = "https://www.schemastore.org/api/json/catalog.json",
+            },
+            completion = true,
+            validate = false,
+            format = { enabled = false },
+            editor = { tabSize = 2 },
+            hover = true,
+            schemas = {
+              kubernetes = {
+                "*.{yml,yaml}",
+              },
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+              ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines*.{yml,yaml}",
+              ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+              ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+              ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+              ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+              ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = {
+                "gitlab-ci*.{yml,yaml}",
+                "*gitlab-ci*.{yml,yaml}",
+              },
+              ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+            },
+
+            -- Enable custom tags for GitLab CI
+            customTags = {
+              "!reference sequence", -- for !reference tags
+            },
+            -- Disable schema validation for specific patterns if needed
+            disableAdditionalProperties = false,
+          },
+        },
+      },
+
+      ansiblels = {
+        filetypes = {
+          "ansible",
+        },
+        settings = {
+          ansible = {
+            ansible = {
+              path = "ansible",
+              useFullyQualifiedCollectionNames = true,
+            },
+            executionEnvironment = {
+              enabled = false,
+            },
+            python = {
+              interpreterPath = "python",
+            },
+            validation = {
+              enabled = false,
+              lint = {
+                enabled = false,
+                path = "ansible-lint",
+              },
+            },
+            completion = {
+              provideRedirectModules = true,
+              provideModuleOptionAliases = true,
+            },
+          },
+        },
+        init_options = {
+          settings = {
+            ansible = {
+              ansible = {
+                path = "ansible",
+              },
+              ansibleLint = {
+                path = "ansible-lint",
+              },
+              collections = {
+                paths = {
+                  "~/.ansible/collections",
+                  "/usr/share/ansible/collections",
+                  "~/.local/share/pipx/venvs/ansible/lib/python3.12/site-packages",
+                },
+              },
+            },
+          },
+        },
+      },
+
+      terraformls = {},
+    },
+  },
+}
