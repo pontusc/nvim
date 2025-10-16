d4a38b42d10510c8db4760623a0d9ce1b25aeb2c
Pontus Curtsson <pontuscurtsson@gmail.com>
2025-10-16 15:51:09 +0200

    Add tmux-navigator plugin for vim->tmux integration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,10 +10,14 @@ vim.keymap.del("n", "<C-Left>")
 vim.keymap.del("n", "<C-Right>")
 
 -- Window navigation
-vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window" })
-vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window" })
-vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window" })
-vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window" })
+vim.keymap.set("n", "<C-Up>", "<Cmd>TmuxNavigateUp<CR>", {})
+vim.keymap.set("n", "<C-Down>", "<Cmd>TmuxNavigateDown<CR>", {})
+vim.keymap.set("n", "<C-Left>", "<Cmd>TmuxNavigateLeft<CR>", {})
+vim.keymap.set("n", "<C-Right>", "<Cmd>TmuxNavigateRight<CR>", {})
+-- vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window" })
+-- vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window" })
+-- vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window" })
+-- vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window" })
 
 -- Window resizing
 vim.keymap.set("n", "<C-A-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
